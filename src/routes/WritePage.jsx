ba8e9c70b9e8c86da5b2d2661690305976008f5e
adeganteng <wicaksonoade21@gmail.com>
2024-12-02 09:17:39 +0700

    Write Page

--- src/routes/WritePage.jsx
+++ src/routes/WritePage.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import { useUser } from "@clerk/clerk-react";
+import { Loader } from "lucide-react";
+import "react-quill-new/dist/quill.snow.css";
+import ReactQuill from "react-quill-new";
 
 const WritePage = () => {
-  return <div>WritePage</div>;
+  const { isLoaded, isSignedIn } = useUser();
+
+  if (!isLoaded) {
+    return (
+      <div className="flex items-center gap-2">
+        Loading...
+        <Loader className="animate-spin" />
+      </div>
+    );
+  }
+
+  if (isLoaded && !isSignedIn) {
+    return <div>You should be signed in to write a post</div>;
+  }
+
+  return (
+    <div className="h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] flex flex-col gap-6 py-8">
+      <h1 className="text-xl font-light">Create a New Post</h1>
+      <form className="flex flex-col gap-6 flex-1 mb-6">
+        <button className="w-max p-2 shadow-md rounded-xl text-sm text-gray-500 bg-white">
+          Add a cover image
+        </button>
+        <input
+          className="text-4xl bg-transparent font-semibold outline-none"
+          type="text"
+          placeholder="My Awesome Story"
+        />
+        <div className="flex items-center gap-4">
+          <label htmlFor="" className="text-sm">
+            Choose a category
+          </label>
+          <select
+            name="cat"
+            id=""
+            className="p-2 rounded-xl bg-white shadow-md outline-none"
+          >
+            <option value="general">General</option>
+            <option value="web-design">Web Design</option>
+            <option value="development">Development</option>
+            <option value="databases">Databases</option>
+            <option value="seo">Search Engines</option>
+            <option value="marketing">Marketing</option>
+          </select>
+        </div>
+        <textarea
+          className="p-4 rounded-xl bg-white shadow-md"
+          placeholder="A Short Description"
+          name="desc"
+        />
+        <ReactQuill
+          theme="snow"
+          className="flex-1 rounded-xl bg-white shadow-md "
+        />
+        <button className="w-36 bg-blue-800 text-white font-medium rounded-xl mt-4 p-2">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default WritePage;
