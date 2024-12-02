38b96994439fb4a92bc38b8c3d5cb61d62328b31
adeganteng <wicaksonoade21@gmail.com>
2024-12-02 10:27:25 +0700

    Tutorial Routes express js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
+import commentRouter from "./routes/comment.route.js";
 
 const app = express();
 
-console.log(process.env.TEST);
+// app.get("/test", (req, res) => {
+//   res.status(200).json({ message: "Hello World" });
+// });
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.use("/users", userRouter);
+app.use("/users", postRouter);
+app.use("/users", commentRouter);
+
+app.listen(3000, () =>
+  console.log(`Server running on port http://localhost:3000`)
+);
